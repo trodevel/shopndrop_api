662be306d8641a19e1bd34520e2b948bf153d927
trodevel <trodevel@gmail.com>
2019-05-11 14:37:29 +0200

    added example

---
+++ examples/example_AddRideRequest_ok_3.php
@@ -0,0 +1,34 @@
+<?php
+// $Revision: 11267 $ $Date:: 2019-05-11 #$ $Author: serge $
+
+require_once __DIR__.'/../api.php';
+require_once __DIR__.'/../helper_add_ride.php';
+require_once __DIR__.'/../../shopndrop_protocol/html_helper_web.php';
+require_once '../credentials.php';
+
+$error_msg = "";
+
+echo "\n";
+echo "TEST: AddRideRequest\n";
+{
+    // create ride
+
+    $plz = 50668; // Center of Cologne
+    $time = time() + 30 * 60;   // 30 min from now
+    $max_weight     = 3.5; // kg
+
+    $ride_id = NULL;
+
+    $res = \shopndrop_api\add_ride_auto( $host, $port, $login, $password, $plz, $time, $max_weight, $ride_id, $resp );
+
+    if( $res == true )
+    {
+        echo "OK: " . \shopndrop_protocol\web\to_html( $resp ) . "\n\n";
+    }
+    else
+    {
+        echo "ERROR: " . \shopndrop_protocol\web\to_html( $resp ) . "\n\n";
+    }
+}
+
+?>
