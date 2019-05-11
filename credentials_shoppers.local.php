04ba793e3c404400d1d57c39d5ca1a623b03921d
trodevel <trodevel@gmail.com>
2019-05-11 15:25:28 +0200

    added credentials for shoppers

---
+++ credentials_shoppers.local.php
@@ -0,0 +1,19 @@
+<?php
+// $Revision: 11268 $ $Date:: 2019-05-11 #$ $Author: serge $
+
+$host = "https://localhost";
+$port = 1234;
+
+$login_1  = "test_shopper_1";
+$login_2  = "test_shopper_2";
+$login_3  = "test_shopper_3";
+$login_4  = "test_shopper_4";
+$login_5  = "test_shopper_5";
+$login_6  = "test_shopper_6";
+$login_7  = "test_shopper_7";
+$login_8  = "test_shopper_8";
+$login_9  = "test_shopper_9";
+$login_10 = "test_shopper_10";
+$password = "xxx";
+
+?>
