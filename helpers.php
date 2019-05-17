0b96ac22abe86e369e2a9d2f5478bd57a1cbba10
trodevel <trodevel@gmail.com>
2019-05-17 03:31:49 +0200

    added convenience header

---
+++ helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+// $Revision: 11481 $ $Date:: 2019-05-17 #$ $Author: serge $
+
+require_once 'helper_add_ride.php';
+require_once 'helper_add_order.php';
+require_once 'helper_get_dash_screen_shopper.php';
+require_once 'helper_get_dash_screen_user.php';
+require_once 'helper_get_product_item_list.php';
+
+?>
