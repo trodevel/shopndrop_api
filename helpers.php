7727e0b3852af55da939dae09acfb89f9565bad9
trodevel <trodevel@gmail.com>
2019-05-19 04:10:53 +0200

    added helpers: accept_order(), decline_order(), get_shopping_request_info()

--- helpers.php
+++ helpers.php
@@ -1,12 +1,15 @@
 <?php
 
-// $Revision: 11513 $ $Date:: 2019-05-17 #$ $Author: serge $
+// $Revision: 11526 $ $Date:: 2019-05-19 #$ $Author: serge $
 
 require_once 'helper_add_ride.php';
 require_once 'helper_add_order.php';
 require_once 'helper_get_dash_screen_shopper.php';
 require_once 'helper_get_dash_screen_user.php';
+require_once 'helper_accept_order.php';
+require_once 'helper_decline_order.php';
 require_once 'helper_get_product_item_list.php';
 require_once 'helper_get_shopping_list_with_totals.php';
+require_once 'helper_get_shopping_request_info.php';
 
 ?>
