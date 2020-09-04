b7cd371e22c822d5bc7a398da7137c69c7a63a9b
trodevel <trodevel@gmail.com>
2020-09-04 03:30:50 +0200

    simplified example

--- examples/example_web_GetProductItemListRequest_ok.php
+++ examples/example_web_GetProductItemListRequest_ok.php
@@ -1,8 +1,8 @@
 <?php
-// $Revision: 11135 $ $Date:: 2019-05-08 #$ $Author: serge $
+// $Revision: 13684 $ $Date:: 2020-09-04 #$ $Author: serge $
 
 require_once __DIR__.'/../api.php';
-require_once __DIR__.'/../../shopndrop_protocol/html_helper_web.php';
+require_once __DIR__.'/../../shopndrop_web_protocol/html_helper.php';
 require_once '../credentials.php';
 
 $error_msg = "";
@@ -11,59 +11,29 @@ $user_id = 0;
 
 echo "\n";
 echo "TEST: GetProductItemListRequest\n";
+try
 {
     $api = new \shopndrop_api\Api( $host, $port );
 
     $session_id = NULL;
 
-    // open session
-    if( $api->open_session( $login, $password, $session_id, $error_msg ) == true )
-    {
-        echo "OK: opened session\n";
+    $api->open_session( $login, $password, $session_id, $error_msg );
 
-        // get user ID
-        $req = new \generic_protocol\GetUserIdRequest( $session_id, $login );
+    $api->get_user_id( $session_id, $login, $user_id, $error_msg );
 
-        echo "REQ = " . $req->to_generic_request() . "\n";
-        $resp = $api->submit( $req );
-        echo "user id = " . $resp->user_id . "\n\n";
-        $user_id = $resp->user_id;
+    echo "user id = " . $user_id . "\n\n";
 
-        // execute request
-        {
-            $req = new \shopndrop_protocol\web\GetProductItemListRequest( $session_id );
+    $resp = NULL;
 
-            echo "REQ = " . $req->to_generic_request() . "\n";
-            $resp = $api->submit( $req );
+    $api->get_product_item_list( $session_id, $resp );
 
-            if( get_class ( $resp ) == "generic_protocol\ErrorResponse" )
-            {
-                echo "ERROR: " . \shopndrop_protocol\web\to_html( $resp ) . "\n\n";
-            }
-            elseif( get_class( $resp ) == "shopndrop_protocol\web\GetProductItemListResponse" )
-            {
-                echo "OK: " . \shopndrop_protocol\web\to_html( $resp ) . "\n\n";
-            }
-            else
-            {
-                echo "ERROR: unknown response: " . get_class( $resp ) . "\n\n";
-            }
-        }
+    echo \shopndrop_web_protocol\to_html( $resp ) . "\n\n";
 
-        // close session
-        if( $api->close_session( $session_id, $error_msg ) == true )
-        {
-            echo "OK: session closed\n";
-        }
-        else
-        {
-            echo "ERROR: cannot close session: $error_msg\n";
-        }
-    }
-    else
-    {
-        echo "ERROR: cannot open session: $error_msg\n";
-    }
+    $api->close_session( $session_id, $error_msg );
+}
+catch( \Exception $e )
+{
+    echo "FATAL: $e\n";
 }
 
 ?>
