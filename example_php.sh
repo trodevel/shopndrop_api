466e2bc4129b10e0da00a4f0c9b6e709c1af14d3
trodevel <trodevel@gmail.com>
2020-09-07 03:10:16 +0200

    parallelized call of firefox

--- example_php.sh
+++ example_php.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 cd examples
-for s in example_*.php; do php $s > a_$s.html; firefox a_$s.html; done;
+for s in example_*.php;
+do
+    php $s > a_$s.html
+    firefox a_$s.html &
+done;
 cd ..
